9c66a905fb6b909f307027c58f573a134d6393e4
mwburwell <mwburwell@gmail.com>
2025-01-31 13:16:21 -0500

    Add mesh to Character and Enemy

--- Source/Aura/Private/Character/AuraCharacterBases.cpp
+++ Source/Aura/Private/Character/AuraCharacterBases.cpp
@@ -5,6 +5,11 @@
 
 AAuraCharacterBases::AAuraCharacterBases()
 {
+	PrimaryActorTick.bCanEverTick = true;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 
