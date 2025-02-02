697dff1fd951240cc7c51629058e9c8ea38947f5
mwburwell <mwburwell@gmail.com>
2025-02-01 19:03:13 -0500

    Init Player State

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copywrite On Draft Mac-N-Cheese
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -0,0 +1,18 @@
+// Copywrite On Draft Mac-N-Cheese
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+};
